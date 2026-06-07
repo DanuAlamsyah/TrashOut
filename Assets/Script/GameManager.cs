d932f7b2d5985850ad541a0ef98e9a58081afea3
HELSA CHRISTABEL HARSONO <christabelhelsa@gmail.com>
2026-06-07 22:55:00 +0700

    Add GameManager script and main menu scene
    - Created GameManager.cs to handle game state and scene transitions.
    - Implemented singleton pattern for GameManager.
    - Added functionality to quit the game or return to the main menu using the Escape key.
    - Included methods for starting a new game, continuing the game, and opening credits.
    - Added mainMenu.unity scene and its corresponding meta file.

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    private void Awake()
+    {
+        // Singleton
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            string currentScene = SceneManager.GetActiveScene().name;
+
+            // Jika sedang di MainMenu -> keluar game
+            if (currentScene == "mainMenu")
+            {
+                Application.Quit();
+                Debug.Log("Quit Game");
+            }
+            // Jika sedang di scene lain -> kembali ke MainMenu
+            else
+            {
+                SceneManager.LoadScene("mainMenu");
+            }
+        }
+    }
+
+    public void NewGame()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void ContinueGame()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void OpenCredits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quit Game");
+    }
+}
